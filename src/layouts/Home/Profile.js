00bfea9bdbf3c4394c2a2801853b05632fc0ebf0
HIMANSHU SAMAL <rahulxamal@gmail.com>
2023-03-05 16:26:48 +0530

    first prod

---
+++ src/layouts/Home/Profile.js
@@ -0,0 +1,104 @@
+/* eslint-disable react/no-unescaped-entities */
+import profileKatakana from 'assets/katakana-profile.svg?url';
+import profileImgLarge from 'assets/profile-large.jpg';
+import profileImgPlaceholder from 'assets/profile-placeholder.jpg';
+import profileImg from 'assets/profile.jpg';
+import { Button } from 'components/Button';
+import { DecoderText } from 'components/DecoderText';
+import { Divider } from 'components/Divider';
+import { Heading } from 'components/Heading';
+import { Image } from 'components/Image';
+import { Link } from 'components/Link';
+import { Section } from 'components/Section';
+import { Text } from 'components/Text';
+import { Transition } from 'components/Transition';
+import { Fragment, useState } from 'react';
+import { media } from 'utils/style';
+import styles from './Profile.module.css';
+
+const ProfileText = ({ visible, titleId }) => (
+  <Fragment>
+    <Heading className={styles.title} data-visible={visible} level={3} id={titleId}>
+      <DecoderText text="Hi I'm Sudhanshu" start={visible} delay={500} />
+    </Heading>
+    <Text className={styles.description} data-visible={visible} size="l" as="p">
+      A student from {' '}
+      <Link href="https://www.paruluniversity.ac.in">Parul University</Link> 3rd Year in Computer Science looking for opportunity 
+      to grow and develop myself by doing team projects. If you’re interested in what tools and software I
+      use check out my <Link href="/uses">uses page</Link>.
+    </Text>
+    <Text className={styles.description} data-visible={visible} size="l" as="p">
+      I've always like to watch movies in my spare time, you can check out my <Link href="https://www.letterboxd.com/xentorio">Letterboxd</Link>.<br /> I like Cats though.if you wanna have chat for cat or projects? feel free to drop me a line.
+    </Text>
+  </Fragment>
+);
+
+export const Profile = ({ id, visible, sectionRef }) => {
+  const [focused, setFocused] = useState(false);
+  const titleId = `${id}-title`;
+
+  return (
+    <Section
+      className={styles.profile}
+      onFocus={() => setFocused(true)}
+      onBlur={() => setFocused(false)}
+      as="section"
+      id={id}
+      ref={sectionRef}
+      aria-labelledby={titleId}
+      tabIndex={-1}
+    >
+      <Transition in={visible || focused} timeout={0}>
+        {visible => (
+          <div className={styles.content}>
+            <div className={styles.column}>
+              <ProfileText visible={visible} titleId={titleId} />
+              <Button
+                secondary
+                className={styles.button}
+                data-visible={visible}
+                href="mailto:rahulxamal@gmail.com"
+                icon="send"
+              >
+                Send me a message
+              </Button>
+            </div>
+            <div className={styles.column}>
+              <div className={styles.tag} aria-hidden>
+                <Divider
+                  notchWidth="64px"
+                  notchHeight="8px"
+                  collapsed={!visible}
+                  collapseDelay={1000}
+                />
+                <div className={styles.tagText} data-visible={visible}>
+                  About Me
+                </div>
+              </div>
+              <div className={styles.image}>
+                <Image
+                  reveal
+                  delay={100}
+                  placeholder={profileImgPlaceholder}
+                  srcSet={[profileImg, profileImgLarge]}
+                  sizes={`(max-width: ${media.mobile}px) 100vw, 480px`}
+                  alt="Profile"
+                />
+                <svg
+                  aria-hidden="true"
+                  width="135"
+                  height="765"
+                  viewBox="0 0 135 765"
+                  className={styles.svg}
+                  data-visible={visible}
+                >
+                  <use href={`${profileKatakana}#katakana-profile`} />
+                </svg>
+              </div>
+            </div>
+          </div>
+        )}
+      </Transition>
+    </Section>
+  );
+};
